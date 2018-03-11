c8bf9429bf2962dc051d78361c8d2c8a65dccc17
Bartek Kryza <bkryza@gmail.com>
2018-03-11 13:37:33 +0100

    VFS-4171 Removed extraneous logging

--- src/cberl.erl
+++ src/cberl.erl
@@ -387,11 +387,10 @@ code_change(_OldVsn, State, _Extra) ->
     cberl_nif:response() | {error, Reason :: term()}.
 call(Connection, Request, Timeout) ->
     Ref = make_ref(),
-    io:format("Making request ~p~n", [Request]),
     gen_server:cast(Connection, {request, Ref, self(), Request}),
     case receive_response(Ref, Timeout) of
-        {ok, ResponseRef} -> io:format("received response ~p~n", [ResponseRef]), receive_response(ResponseRef, Timeout);
-        {error, Reason} ->io:format("received error ~p~n", [Reason]),  {error, Reason}
+        {ok, ResponseRef} -> receive_response(ResponseRef, Timeout);
+        {error, Reason} -> {error, Reason}
     end.
 
 %%--------------------------------------------------------------------
